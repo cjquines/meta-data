069b0bddfeb23a9e3006538b98b2286950b5d583
CJ Quines <cj@cjquines.com>
2024-02-05 22:43:22 -0500

    more

--- src/App.js
+++ src/App.js
@@ -3,6 +3,148 @@ import "./App.scss";
 
 import * as metadata from "./metadata.json";
 
+const FilterOptions = ({ label, options, isSelected, onToggle }) => {
+  let hasSelected = false;
+
+  const detailsDom = options.map((option, i) => {
+    const selected = isSelected(option);
+    hasSelected |= selected;
+    return (
+      <div
+        className={selected ? "selected" : ""}
+        key={i}
+        onClick={() => onToggle(option, selected)}
+      >
+        {option}
+      </div>
+    );
+  });
+
+  return (
+    <details>
+      <summary>
+        <span className={hasSelected ? "selected" : ""}>{label}</span>
+      </summary>
+      <div className="filter">{detailsDom}</div>
+    </details>
+  );
+};
+
+const FeedersFilter = ({ filter }) => {
+  const { filters, setFilters } = filter;
+  const hasSelected = filters.feeders.min || filters.feeders.max;
+  const minInput = useRef();
+  const maxInput = useRef();
+
+  return (
+    <details>
+      <summary>
+        <span className={hasSelected ? "selected" : ""}>
+          Has number of feeders:
+        </span>
+      </summary>
+      <div className="filter">
+        at least
+        <input
+          ref={minInput}
+          type="number"
+          value={filters.feeders.min}
+          onChange={(e) =>
+            setFilters({
+              ...filters,
+              feeders: { ...filters.feeders, min: e.target.valueAsNumber },
+            })
+          }
+        />
+        and at most
+        <input
+          ref={maxInput}
+          type="number"
+          value={filters.feeders.max}
+          onChange={(e) =>
+            setFilters({
+              ...filters,
+              feeders: { ...filters.feeders, max: e.target.valueAsNumber },
+            })
+          }
+        />
+        <button
+          className="secondary"
+          onClick={() => {
+            setFilters({ ...filters, feeders: {} });
+            minInput.current.value = undefined;
+            maxInput.current.value = undefined;
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </details>
+  );
+};
+
+const Filters = ({ filter }) => {
+  const { allFilters, filters, setFilters } = filter;
+  const all = allFilters.current;
+
+  return (
+    <div className="filters">
+      <FilterOptions
+        label="From hunt:"
+        options={all.source.hunt}
+        isSelected={(hunt) => filters.source.hunt === hunt}
+        onToggle={(hunt, selected) =>
+          setFilters({
+            ...filters,
+            source: { ...filters.source, hunt: selected ? undefined : hunt },
+          })
+        }
+      />
+      <FilterOptions
+        label="From year:"
+        options={all.source.year}
+        isSelected={(year) => filters.source.year === year}
+        onToggle={(year, selected) =>
+          setFilters({
+            ...filters,
+            source: { ...filters.source, year: selected ? undefined : year },
+          })
+        }
+      />
+      <FilterOptions
+        label="Has all the tags:"
+        options={all.tags}
+        isSelected={(tag) => filters.tags.includes(tag)}
+        onToggle={(tag, selected) =>
+          setFilters({
+            ...filters,
+            tags: selected
+              ? filters.tags.filter((other) => other !== tag)
+              : [...filters.tags, tag],
+          })
+        }
+      />
+      <FilterOptions
+        label="Has note:"
+        options={["yes", "no"]}
+        isSelected={(opt) => filters.note === opt}
+        onToggle={(opt, selected) =>
+          setFilters({ ...filters, note: selected ? undefined : opt })
+        }
+      />
+      <FilterOptions
+        label="Has flavor:"
+        options={["yes", "no"]}
+        isSelected={(opt) => filters.flavor === opt}
+        onToggle={(opt, selected) =>
+          setFilters({ ...filters, flavor: selected ? undefined : opt })
+        }
+      />
+      <FeedersFilter filter={filter} />
+    </div>
+  );
+};
+
 const Spoiler = (props) => {
   const [visible, setVisible] = useState(false);
   return (
@@ -36,102 +178,6 @@ const Tags = ({ idx, tags }) => {
   );
 };
 
-const Filters = ({ filter }) => {
-  const { allFilters, filters, setFilters } = filter;
-  const all = allFilters.current;
-
-  const tagsDom = all.tags.map((tag, i) => {
-    const selected = filters.tags.includes(tag);
-    return (
-      <div
-        className={selected ? "selected" : ""}
-        key={i}
-        onClick={() =>
-          setFilters(() => ({
-            ...filters,
-            tags: selected
-              ? filters.tags.filter((other) => other !== tag)
-              : [...filters.tags, tag],
-          }))
-        }
-      >
-        {tag}
-      </div>
-    );
-  });
-
-  const huntDom = all.source.hunt.map((hunt, i) => {
-    const selected = filters.source.hunt === hunt;
-    return (
-      <div
-        className={selected ? "selected" : ""}
-        key={i}
-        onClick={() =>
-          setFilters(() => ({
-            ...filters,
-            source: {
-              ...filters.source,
-              hunt: selected ? undefined : hunt,
-            },
-          }))
-        }
-      >
-        {hunt}
-      </div>
-    );
-  });
-
-  const yearDom = all.source.year.map((year, i) => {
-    const selected = filters.source.year === year;
-    return (
-      <div
-        className={selected ? "selected" : ""}
-        key={i}
-        onClick={() =>
-          setFilters(() => ({
-            ...filters,
-            source: {
-              ...filters.source,
-              year: selected ? undefined : year,
-            },
-          }))
-        }
-      >
-        {year}
-      </div>
-    );
-  });
-
-  return (
-    <div className="filters">
-      <details>
-        <summary>
-          <span className={filters.tags.length > 0 ? "selected" : ""}>
-            Has all these tags:
-          </span>
-        </summary>
-        <div className="filter">{tagsDom}</div>
-      </details>
-      <details>
-        <summary>
-          <span className={filters.source.hunt ? "selected" : ""}>
-            From hunt:
-          </span>
-        </summary>
-        <div className="filter">{huntDom}</div>
-      </details>
-      <details>
-        <summary>
-          <span className={filters.source.year ? "selected" : ""}>
-            From year:
-          </span>
-        </summary>
-        <div className="filter">{yearDom}</div>
-      </details>
-    </div>
-  );
-};
-
 const Info = ({ filter, idx, next, tags, source }) => {
   const [open, setOpen] = useState(false);
 
@@ -151,8 +197,10 @@ const Info = ({ filter, idx, next, tags, source }) => {
           <Tags idx={idx} tags={tags} />
         </div>
         <div className="buttons">
+          <button className="secondary" onClick={() => setOpen(!open)}>
+            Filter
+          </button>
           <button onClick={() => next()}>New meta</button>
-          <button onClick={() => setOpen(!open)}>Filter</button>
         </div>
       </div>
       {open ? <Filters filter={filter} /> : null}
@@ -230,41 +278,41 @@ const shuffle = (arr) => {
 
 const getFilters = (metas) => {
   const res = {
-    tags: new Set(),
     source: {
       hunt: new Set(),
       year: new Set(),
     },
+    tags: new Set(),
   };
 
   for (const meta of metas) {
+    res.source.hunt.add(meta.source.hunt);
+    res.source.year.add(meta.source.year);
     for (const tag of meta.tags) {
       res.tags.add(tag);
     }
-    res.source.hunt.add(meta.source.hunt);
-    res.source.year.add(meta.source.year);
   }
 
   return {
-    tags: Array.from(res.tags).sort(),
     source: {
       hunt: Array.from(res.source.hunt).sort(),
       year: Array.from(res.source.year).sort((a, b) => a - b),
     },
+    tags: Array.from(res.tags).sort(),
   };
 };
 
 const matches = (meta, filters) => {
   return [
-    !filters.feeders.min || meta.feeders.length >= filters.feeders.min,
-    !filters.feeders.max || meta.feeders.length <= filters.feeders.max,
-    filters.flavor !== "yes" || meta.flavor,
-    filters.flavor !== "no" || !meta.flavor,
-    filters.note !== "yes" || meta.note,
-    filters.note !== "no" || !meta.note,
-    filters.tags.every((tag) => meta.tags.includes(tag)),
     !filters.source.hunt || filters.source.hunt === meta.source.hunt,
     !filters.source.year || filters.source.year === meta.source.year,
+    filters.tags.every((tag) => meta.tags.includes(tag)),
+    filters.note !== "yes" || meta.note,
+    filters.note !== "no" || !meta.note,
+    filters.flavor !== "yes" || meta.flavor,
+    filters.flavor !== "no" || !meta.flavor,
+    !filters.feeders.min || meta.feeders.length >= filters.feeders.min,
+    !filters.feeders.max || meta.feeders.length <= filters.feeders.max,
   ].every(Boolean);
 };
 
@@ -280,7 +328,7 @@ const App = () => {
 
   const [idx, setIdx] = useState(0);
   const [meta, setMeta] = useState(metas.current[0]);
-  const [filters, setFilters] = useState({ feeders: {}, source: {}, tags: [] });
+  const [filters, setFilters] = useState({ source: {}, tags: [], feeders: {} });
   const filter = { allFilters, filters, setFilters };
 
   const next = () => {
